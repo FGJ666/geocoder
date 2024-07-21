16477117c88da2ab158de004ebee79a98bff726b
FGJ <m_sozonov@outlook.com>
2024-07-22 01:48:56 +0300

    new tests

--- test_geocoder.py
+++ test_geocoder.py
@@ -2,4 +2,14 @@
 import pandas as pd
 from geocoder import load_data, geocode_addresses, save_data
 
-print(geocode_addresses(pd.read_csv(r'geocoder\address_test.csv')))
+try:
+    print(load_data(r'geocoder\address_test.csv'))
+    print('\n\n', 130 * '_')
+except:
+    print('ERROR')
+
+try:
+    print(geocode_addresses(pd.read_csv(r'geocoder\address_test.csv')))
+    print('\n\n', 130 * '_')
+except:
+    print('ERROR')
